935507f72d1e96769eef18cfa4be1f93fc8c2049
Acezorey <ace.reyes0213@gmail.com>
2024-12-13 03:04:24 -1000

    Created loss and accuracy functions

--- neurons.py
+++ neurons.py
@@ -21,6 +21,24 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategorialCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis = 1)
+        
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2, 3)
@@ -29,6 +47,8 @@ activation1 = Activation_ReLU()
 dense2 = Layer_Dense(3, 3)
 activation2 = Activation_Softmax()
 
+loss_function = Loss_CategorialCrossentropy()
+
 dense1.forward(X)
 activation1.forward(dense1.output)
 
@@ -36,3 +56,14 @@ dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
 print(activation2.output[:5])
+
+loss = loss_function.calculate(activation2.output, y)
+
+print('loss: ', loss)
+
+predictions = np.argmax(activation2.output, axis = 1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis = 1)
+accuracy = np.mean(predictions == y)
+
+print('acc: ', accuracy)
